c05dd31ca10279337c089ed0176ec3af95819077
Hexenone <101699955+Hexenone@users.noreply.github.com>
2024-03-31 23:37:10 +0700

    hme

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/jobs/jobs/venturer.ts
@@ -0,0 +1,10 @@
+import { Job } from "../base";
+import { Engineering } from "../departments";
+
+const RedZek: Job = {
+  name: "Red Zek",
+  description: "He fights.",
+  department: Engineering,
+};
+
+export default RedZek;
